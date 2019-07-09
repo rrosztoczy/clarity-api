cef98a13aae5d7e5eb00eedf372c1c0d1384498b
rrosztoczy <rrosztoczy@gmail.com>
2019-07-09 11:48:06 -0600

    initial model tests added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #test for association
+  it { should have_many(:thoughts).dependent(:destroy) }
+
+  #Validate key items exist
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+
+  #Validate unique username, email and pw
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:email) }
+  #TODO: Maybe add PD in here?
 end
