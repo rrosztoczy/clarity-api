cfcaf09c77f88699b3f7e31ad9126c4f61cc2783
rrosztoczy <rrosztoczy@gmail.com>
2019-07-09 13:15:01 -0600

    first tests added and passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :thoughts, dependent: :destroy
+    validates_presence_of :username, :first_name, :last_name, :email, :password_digest
+    validates_uniqueness_of :username, :email
 end
