dfd0b99c7f358f0fc1263a33cbbfcc6b76f2f39d
rrosztoczy <rrosztoczy@gmail.com>
2019-07-09 14:38:11 -0600

    added request specs, helper methods, passed all specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
     factory :user do
       username { Faker::Internet.username }
       first_name { Faker::Name.first_name }
-      lase_name { Faker::Name.last_name }
+      last_name { Faker::Name.last_name }
       email { Faker::Internet.safe_email }
+      password_digest { Faker::Internet.password }
     end
   end
