2b5eb8fa0db5f43a1c39f855e0c0c32426b9cdb9
rrosztoczy <rrosztoczy@gmail.com>
2019-07-11 13:49:34 -0600

    implemented fast json serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ module Api
     # GET /users
     def index
         @users = User.all
-        json_response(@users)
+        render json: UserSerializer.new(@users)
     end
 
     # POST /users
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :first_name, :last_name, :email
-  has_many :thoughts
+  # has_many :thoughts
+
+  attribute :thoughts_attributes do |user|
+      user.thoughts.map do |thought|
+        {id: thought.id, quality: thought.quality}
+      end 
+  end 
 end
